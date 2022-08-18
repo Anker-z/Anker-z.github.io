2840c34df1f5d300b253b40ca039b2caa3350820
Anker <11445565+anker-z@user.noreply.gitee.com>
2022-08-18 20:19:50 +0800

    Site updated: 2022-08-18 20:19:50

--- js/global/darkmode.js
+++ js/global/darkmode.js
@@ -1,16 +1,17 @@
-(function(){
-    let nav = document.getElementById('nav');
-    let sidebar = document.createElement('DIV');
-    sidebar.className = 'sidebar-icon';
-    sidebar.innerHTML = '<div><i class="fas fa-adjust"></i></div>';
-    sidebar.onclick = function(){
-        let html = document.getElementsByTagName('html')[0];
-        let dataTheme = html.getAttribute('data-theme');
-        let sidebarFlag = dataTheme === 'dark'?'light':'dark';
-        sidebar.setAttribute('data-theme',sidebarFlag)
-        html.setAttribute('data-theme',sidebarFlag)
-        saveToLocal.set('theme', sidebarFlag, 2)
-    };
-    nav.appendChild(sidebar)
+(function () {
+//   let nav = document.getElementById("nav");
+//   let sidebar = document.createElement("DIV");
+//   sidebar.className = "sidebar-icon";
+//   sidebar.innerHTML = '<div><i class="fas fa-adjust"></i></div>';
+   let html = document.getElementsByTagName("html")[0];
+let darkmodeCopy = document.getElementById('darkmodeCopy')
+  darkmodeCopy.onclick = function () {
+    let dataTheme = html.getAttribute("data-theme");
+    let sidebarFlag = dataTheme === "dark" ? "light" : "dark";
+    html.setAttribute("data-theme", sidebarFlag);
+    saveToLocal.set("theme", sidebarFlag, 2);
+  };
+//   nav.appendChild(sidebar);
 
-})()
+
+})();
